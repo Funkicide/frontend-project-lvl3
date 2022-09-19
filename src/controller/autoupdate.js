a028047ebf508277c63be803c9224c85df392257
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-09-19 13:49:11 +0300

    Refactor: remove if else & move setTimeout

--- src/controller/autoupdate.js
+++ src/controller/autoupdate.js
@@ -1,27 +1,21 @@
 import axios from 'axios';
-import { isEmpty } from 'lodash';
 import makeProxyUrl from './makeProxyUrl.js';
 import parseData from './parseData.js';
 import normalizeRss from './normalizeRss.js';
 
-const autoupdate = (watchedState, milliseconds = 5000) => {
-  if (isEmpty(watchedState.data.activeFeeds)) {
-    setTimeout(() => autoupdate(watchedState), 0);
-  } else {
-    setTimeout(() => {
-      const promises = watchedState.data.activeFeeds.map((item) => {
-        watchedState.processState = 'updating';
-        return axios.get(makeProxyUrl(item))
-          .then(({ data }) => parseData(data.contents))
-          .then((parsedRss) => {
-            normalizeRss(watchedState, parsedRss);
-            watchedState.processState = 'updated';
-          });
-      });
-      Promise.allSettled(promises)
-        .finally(() => autoupdate(watchedState));
-    }, milliseconds);
-  }
+const autoupdate = (watchedState, delay = 5000) => {
+  const promises = watchedState.data.activeFeeds.map((item) => {
+    watchedState.processState = 'updating';
+    return axios.get(makeProxyUrl(item))
+      .then(({ data }) => parseData(data.contents))
+      .then((parsedRss) => {
+        normalizeRss(watchedState, parsedRss);
+        watchedState.processState = 'updated';
+      })
+      .catch((error) => console.log(error.message));
+  });
+  Promise.all(promises)
+    .finally(() => setTimeout(() => autoupdate(watchedState), delay));
 };
 
 export default autoupdate;
