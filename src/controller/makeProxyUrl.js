c65f0ffc6ee080c2f39489db2867d9362913ecd0
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-09-19 13:50:15 +0300

    Refactor: remove needless interpolation

--- src/controller/makeProxyUrl.js
+++ src/controller/makeProxyUrl.js
@@ -1,8 +1,10 @@
-export default (url) => {
+const makeProxyUrl = (url) => {
   const proxyUrl = new URL('https://allorigins.hexlet.app/');
   proxyUrl.pathname = 'get';
-  proxyUrl.searchParams.append('url', `${url}`);
+  proxyUrl.searchParams.append('url', url);
   proxyUrl.searchParams.append('disableCache', 'true');
 
   return proxyUrl;
 };
+
+export default makeProxyUrl;
